c62debb1754f18e619576b8b3c1b3e0af796fccf
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2022-02-10 12:40:59 +0530

    added previous and next buttons

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -6,11 +6,11 @@ export class Newsitems extends Component {
         return (
           
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageurl} style={{height:'200px'}} className="card-img-top" alt="..." />
+                    <img src={!imageurl ? 'https://images.indianexpress.com/2022/02/PK-20220207-DU-Protest-2.jpg': imageurl} style={{height:'200px'}} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={url} className="btn my-1 btn-primary">Read More</a>
+                        <a href={url} className="btn my-1 btn-dark">Read More</a>
                     </div>
                 </div>
       
