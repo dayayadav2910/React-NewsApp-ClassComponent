0215120481dd9f95ddfbe7915192777ee1b9c448
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2022-02-11 11:29:06 +0530

    Routes, top-loader, heading-change are added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+import LoadingBar from 'react-top-loading-bar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+        <Navbar> </Navbar>
+        <LoadingBar
+        color='#f11946'
+        progress={100}
+       
+      />
+
+          <Routes>
+          <Route exact path="/" element={<News key="general" category="general"></News>}></Route>
+          <Route exact path="/entertainment" element={<News key="entertainment" category="entertainment"></News>}></Route>
+          <Route exact path="/business" element={<News key="business" category="business"></News>}></Route>
+          <Route exact path="/health" element={<News key="health"  category="health"></News>}></Route>
+          <Route exact path="/science" element={<News key="science" category="science"></News>}></Route> 
+          <Route exact path="/sports" element={<News key="sports" category="sports"></News>}></Route>
+          <Route exact path="/technology" element={<News key="technology"  category="technology"></News>}></Route>
+          </Routes>
+         
+        </Router>        
+
+      </div>
+    )
+  }
 }
-
-export default App;
--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import Newsitems from './Newsitems'
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export class News extends Component {
+
+    static defaultProps = {
+        category : "General",
+        // pagesize : "5"
+    }
+
+      static propTypes = {
+            // pagesize : PropTypes.number,
+            category: PropTypes.string,
+      }
+
     articles = []
 
     async componentDidMount() {
-        let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=7627c9b831b549a189503486a1a96f64&page=1pageSize=20';
+        let url =`https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=7627c9b831b549a189503486a1a96f64&page=1&pageSize=20`;
         let data = await fetch(url);
         let parsedata = await data.json();
         this.setState({ articles: parsedata.articles, totalResults: parsedata.totalResults })
@@ -22,7 +36,7 @@ export class News extends Component {
         }
     }
     handlePreviousClick = async ()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7627c9b831b549a189503486a1a96f64&page=${this.state.page-1}&pageSize=20`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=7627c9b831b549a189503486a1a96f64&page=${this.state.page-1}&pageSize=20`;
         let data = await fetch(url);
         let parsedata = await data.json();
 
@@ -37,7 +51,7 @@ export class News extends Component {
 
         }
         else{
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7627c9b831b549a189503486a1a96f64&page=${this.state.page+1}&pageSize=20`;
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=7627c9b831b549a189503486a1a96f64&page=${this.state.page+1}&pageSize=20`;
             let data = await fetch(url);
             let parsedata = await data.json();
 
@@ -54,15 +68,23 @@ export class News extends Component {
         console.log("render");
         return (
             <div className='container my-3'>
-                <h1>News Monkey - Top Headlines</h1>
+                <h1 className='text-center' style={{margin: '40px 0px', marginTop:'100px'}}>News Monkey - Top Headlines of {this.props.category}</h1>
+                <InfiniteScroll
+                        dataLength={this.state.articles.length}
+                        next={this.fetchMoreData}
+                        hasMore={this.state.articles.length!== this.totalResults}
+                        loader={<h4>Loading...</h4>}
+                        >
                 <div className="row">
                     {this.state.articles.map((element) => {
                         return (<div className="col-md-4 my-2">
                             <Newsitems title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageurl={element.urlToImage} url={element.url} />
                         </div>)
                     })}
+                  
 
                 </div>
+                </InfiniteScroll>
 
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page<=1} type="button"   className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
